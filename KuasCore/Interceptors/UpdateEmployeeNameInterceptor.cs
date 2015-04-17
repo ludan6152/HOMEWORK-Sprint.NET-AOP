e1ebc4341d7400dea3a32c1c6e138252243c1918
s1101137113 <ludan6152@gmail.com>
2015-04-17 17:48:42 +0800

    https://github.com/s1101137113/HOMEWORK-Sprint.NET-AOP.git

---
+++ KuasCore/Interceptors/UpdateEmployeeNameInterceptor.cs
@@ -0,0 +1,32 @@
+﻿using AopAlliance.Intercept;
+using KuasCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuasCore.Interceptors
+{
+    class UpdateEmployeeNameInterceptor : IMethodInterceptor
+    {
+
+        public object Invoke(IMethodInvocation invocation)
+        {
+            Console.WriteLine("UpdateEmployeeNameInterceptor 攔截到一個方法呼叫 = [{0}]", invocation);
+            Debug.Print("UpdateEmployeeNameInterceptor 攔截到一個方法呼叫 = [{0}]", invocation);
+
+            object result = invocation.Proceed();
+
+            if (result is Employee)
+            {
+                Employee employee = (Employee)result;
+                employee.Name = employee.Name + "是你的好朋友";
+                result = employee;
+            }
+
+            return result;
+        }
+    }
+}
